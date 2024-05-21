32c4443f84a37944c49d289f3c0cbdd7ea8209c3
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-21 17:41:38 -0300

    create and implemente getter and setter for class

---
+++ src/praticeChallenge/AcountBank.java
@@ -0,0 +1,23 @@
+package praticeChallenge;
+
+public class AcountBank {
+    private int numberOfCount;
+    private double balance;
+    public String holder;
+
+    public int getNumberOfCount() {
+        return numberOfCount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setNumberOfCount(int numberOfCount) {
+        this.numberOfCount = numberOfCount;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
