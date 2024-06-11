bd25acfd8af074866c3e4d06f70bc4eca9beaeb7
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-06-10 22:24:43 -0300

    implemente Interface to calc Area

---
+++ src/praticeChallenge/calculadorRetangular/Test.java
@@ -0,0 +1,9 @@
+package praticeChallenge.calculadorRetangular;
+
+public class Test {
+    public static void main(String[] args) {
+        CalculadoraSalaRetangular calcular = new CalculadoraSalaRetangular();
+        calcular.calcularArea(10, 20);
+        calcular.calcularPerimetro(20, 8);
+    }
+}
